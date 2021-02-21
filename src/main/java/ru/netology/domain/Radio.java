1395715a74e0f30f40f9c06afd13e1c88c402733
Andrey Shitikov <andrey.shitikoff@gmail.com>
2021-02-21 14:54:21 +0300

    add class Radio

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,23 @@
+package ru.netology.domain;
+
+public class Radio {
+    private String name;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int currentStation;
+    private boolean on;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+    private int currentVolume;
+    private boolean next = true;
+    private boolean prev = true;
+    private boolean plus = true;
+    private boolean minus = true;
+
+
+
+
+
+
+
+}
