631ad8b4cba10b6cf4a1e284e25c15ea16be8e44
Andrey Shitikov <andrey.shitikoff@gmail.com>
2021-02-21 15:44:00 +0300

    add Test

---
+++ src/test/java/ru/netology/Domain/RadioTest.java
@@ -0,0 +1,64 @@
+package ru.netology.Domain;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Radio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RadioTest {
+
+    @Test
+    void shouldUpStation(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.changeNextStation();
+        assertEquals(0,radio.getCurrentStation());
+
+        radio.setCurrentStation(5);
+        radio.changeNextStation();
+        assertEquals(6,radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldDownStation(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.changePrevStation();
+        assertEquals(9,radio.getCurrentStation());
+
+        radio.setCurrentStation(7);
+        radio.changePrevStation();
+        assertEquals(6,radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldVolumeUp(){
+        Radio radio = new Radio();
+        radio.setCurrentVolume(10);
+        radio.volumeUp();
+        assertEquals(10,radio.getCurrentVolume());
+
+        radio.setCurrentVolume(3);
+        radio.volumeUp();
+        assertEquals(4,radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldVolumeDown(){
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.volumeDown();
+        assertEquals(0,radio.getCurrentVolume());
+
+        radio.setCurrentVolume(6);
+        radio.volumeDown();
+        assertEquals(5,radio.getCurrentVolume());
+
+    }
+
+
+
+
+
+
+}
