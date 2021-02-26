94691114e8ca05d514fa73111a4dc0b94c622508
Andrey Shitikov <andrey.shitikoff@gmail.com>
2021-02-26 19:10:01 +0300

    Updated RadioTest

--- src/test/java/ru/netology/Domain/RadioTest.java
+++ src/test/java/ru/netology/Domain/RadioTest.java
@@ -8,57 +8,66 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RadioTest {
 
     @Test
-    void shouldUpStation(){
+    void shouldUpStation() {
         Radio radio = new Radio();
         radio.setCurrentStation(9);
         radio.changeNextStation();
-        assertEquals(0,radio.getCurrentStation());
+        assertEquals(0, radio.getCurrentStation());
+    }
 
+    @Test
+    void shouldUpStationMiddle(){
+        Radio radio = new Radio();
         radio.setCurrentStation(5);
         radio.changeNextStation();
         assertEquals(6,radio.getCurrentStation());
     }
 
     @Test
-    void shouldDownStation(){
+    void shouldDownStation() {
         Radio radio = new Radio();
         radio.setCurrentStation(0);
         radio.changePrevStation();
-        assertEquals(9,radio.getCurrentStation());
+        assertEquals(9, radio.getCurrentStation());
+    }
 
+    @Test
+        void shouldDownStationMiddle(){
+        Radio radio = new Radio();
         radio.setCurrentStation(7);
         radio.changePrevStation();
         assertEquals(6,radio.getCurrentStation());
     }
 
     @Test
-    void shouldVolumeUp(){
+    void shouldVolumeUp() {
         Radio radio = new Radio();
         radio.setCurrentVolume(10);
         radio.volumeUp();
-        assertEquals(10,radio.getCurrentVolume());
+        assertEquals(10, radio.getCurrentVolume());
+    }
 
+    @Test
+        void shouldVolumeUpMiddle(){
+        Radio radio = new Radio();
         radio.setCurrentVolume(3);
         radio.volumeUp();
         assertEquals(4,radio.getCurrentVolume());
     }
 
     @Test
-    void shouldVolumeDown(){
+    void shouldVolumeDown() {
         Radio radio = new Radio();
         radio.setCurrentVolume(0);
         radio.volumeDown();
-        assertEquals(0,radio.getCurrentVolume());
+        assertEquals(0, radio.getCurrentVolume());
+    }
 
+    @Test
+        void shouldVolumeDownUpMiddle(){
+        Radio radio = new Radio();
         radio.setCurrentVolume(6);
         radio.volumeDown();
         assertEquals(5,radio.getCurrentVolume());
-
     }
-
-
-
-
-
-
 }
