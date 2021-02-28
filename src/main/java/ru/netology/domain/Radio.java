95ac8a1275826a450595de8fd6a7d934d4d3b764
Andrey Shitikov <andrey.shitikoff@gmail.com>
2021-02-28 22:54:35 +0300

    Updated Radio.java

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -14,80 +14,12 @@ public class Radio {
     private boolean plus = true;
     private boolean minus = true;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getMaxStation() {
-        return maxStation;
-    }
-
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-
-    public int getMinStation() {
-        return minStation;
-    }
-
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-
     public int getCurrentStation() {
         return currentStation;
     }
 
     public void setCurrentStation(int currentStation) {
         this.currentStation = currentStation;
-    }
-
-    public boolean isOn() {
-        return on;
-    }
-
-    public void setOn(boolean on) {
-        this.on = on;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-    public void volumeUp(){
-        if (currentVolume >= maxVolume){
-            return;
-        }
-        currentVolume ++;
-    }
-    public void volumeDown(){
-        if (currentVolume <= minVolume){
-            return;
-        }
-        currentVolume --;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        this.currentVolume = currentVolume;
     }
 
     public void changeNextStation(){
@@ -103,19 +35,25 @@ public class Radio {
         }
     }
 
-    public boolean isPlus() {
-        return plus;
+    public int getCurrentVolume() {
+        return currentVolume;
     }
 
-    public void setPlus(boolean plus) {
-        this.plus = plus;
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
     }
 
-    public boolean isMinus() {
-        return minus;
+    public void volumeUp(){
+        if (currentVolume >= maxVolume){
+            return;
+        }
+        currentVolume ++;
+    }
+    public void volumeDown(){
+        if (currentVolume <= minVolume){
+            return;
+        }
+        currentVolume --;
     }
 
-    public void setMinus(boolean minus) {
-        this.minus = minus;
-    }
 }
